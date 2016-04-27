d193a0c34f0b10dabdb83061f969f3f559e782ba
heoegema <hollyoegema@gmail.com>
2016-04-27 16:35:08 -0400

    Added PaperClip Gem

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,14 @@
+class PostsController < ApplicationController
+
+  def index
+  end
+  class Post < ActiveRecord::Base  
+    validates :image, presence: true
+
+    has_attached_file :image, styles: { :medium => "640x" }
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  end  
+
+end 
+
+
