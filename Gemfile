658e67233e41de092d7346fd2a06310953b3676d
heoegema <hollyoegema@gmail.com>
2016-04-27 22:06:39 -0400

    updated index etc.

--- Gemfile
+++ Gemfile
@@ -13,7 +13,14 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+
+#PaperClip
 gem "paperclip", "~> 5.0.0.beta1"
+
+#Simple Form
+gem 'simple_form', '~> 3.1.0'  
+#Boostrap/Sass
+gem 'bootstrap-sass', '~> 3.3.6'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,34 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
   end
-  class Post < ActiveRecord::Base  
-    validates :image, presence: true
-
-    has_attached_file :image, styles: { :medium => "640x" }
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  end  
-
+  
+  def new
+  
+    @post = Post.new 
+  end 
+  
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
+  end 
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:image, :caption)
+   
+  end 
+  
 end 
 
+class Post < ActiveRecord::Base  
+  validates :image, presence: true
+
+  has_attached_file :image, styles: { :medium => "640x" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  
+  
+end  
 
