f6530378d5d19a0c72a35465a5284f84f7992d1b
heoegema <hollyoegema@gmail.com>
2016-04-27 23:03:20 -0400

    Updated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,56 @@
-class PostsController < ApplicationController
+class PostsController < ApplicationController  
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
   end
-  
+
+  def show
+  end
+
   def new
-  
-    @post = Post.new 
-  end 
-  
+    @post = Post.new
+  end
+
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
-  end 
-  
+    if @post = Post.create(post_params)
+      flash[:succes] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created! Please try again!"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:succes] = "Post successfully updated!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Update failed. Please try again!"
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:succes] = "Your post has been deleted";
+    redirect_to root_path
+  end
+
   private
-  
+
   def post_params
     params.require(:post).permit(:image, :caption)
-   
-  end 
-  
-end 
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end  
 
 class Post < ActiveRecord::Base  
   validates :image, presence: true
